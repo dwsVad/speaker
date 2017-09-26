8e12bb9267b99ca2c3e508bee2365e9c92662647
@Vad <vadim.protcenko@corp.hitwe.com>
2017-09-26 17:30:04 +0300

    init

---
+++ src/Speaker.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: developer
+ * Date: 26.09.17
+ * Time: 16:46
+ */
+class Speaker
+{
+    public static function say(string $string) :string {
+        return $string;
+    }
+}
